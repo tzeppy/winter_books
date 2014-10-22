803d2ef22690f66a79cd3a4bedd6ee9aacef66aa
tzeppy <tzeppy@gmail.com>
2014-10-21 22:10:43 -0700

    add eva

---
+++ eva.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+  <head>
+    <title>WinterBooks</title>
+    <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="static/css/site.css" type="text/css" charset="utf-8"></link>
+    <link rel="stylesheet" href="static/css/eva.css" type="text/css" charset="utf-8"></link>
+
+  </head>
+  <body>
+    <?php include('banner.php'); ?>
+
+    <div class="content" id="eva_content">
+    	 <img id="eva_image" src="static/images/eva.jpg"/>
+    	 Carmine is dedicated to my great friend Christina Newman. 
+	 She was the one who encouraged me to "write a book".
+	 And I believe that my protagonist Ona Bell embodies to some degree Christina's fighting spirit.
+	 This page is devoted to the story of Christina and her daughter Eva.
+ 	 Out of every ten dollars of profit from Carmine, I'm giving one dollar to Eva's educational fund.
+	 Christina has chosen me to be Eva's Godmother. I think that there are two major gifts I can
+	 give Eva - my love, and Education. (The full story is coming up shortly)
+
+    </div>
+  </body>
+</html>
