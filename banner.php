f3fcd0ab6ec07908a60144e67fb896e8569b6006
tzeppy <tzeppy@gmail.com>
2014-10-25 19:50:10 -0700

    eva to ava

--- banner.php
+++ banner.php
@@ -7,7 +7,7 @@
         <li><a href="books.php">BOOKS</a></lu>
         <li><a href="about.php">ABOUT</a></lu>
         <li><a href="blogs.php">BLOGS</a></lu>
-	<li><a href="eva.php">EVA</a></lu>
+	<li><a href="ava.php">AVA</a></lu>
         <!--li><a href="news.php">NEWS</a></lu-->
         <!--li><a href="contact.php">CONTACT</a></lu-->
       </ul>
