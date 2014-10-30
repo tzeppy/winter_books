9acf7b5125654d684f62543ef46e4328a3b34c83
tzeppy <tzeppy@gmail.com>
2014-10-29 20:25:30 -0700

    ava mods

--- ava.php
+++ ava.php
@@ -40,29 +40,12 @@
 	 <br>
 
 	 <h2><center>Ava's Story</center></h2>
-	 <p>Ava is a miracle child of Christina Newman and Ernesto Monne. Her parents were married in April 26 2008.
-	 They wished for a child but for a while they had none. Then in December 2010 a tragedy struck the family - Christina was diagnosed with tripple negative breast cancer.
-	 She underwent surgeries and chemotherapy. She was told that she won't be able to have children due to the side effects of the treatment.
-	 In September 2012 her chemo was over, but she continued taking a parp inhibitor. She felt sick and didn't understand why she can't loose weight.
-	 The doctors kept telling her it was due to the chemo's side effects. In February 2013 Christina was declared cancer free.
-	 She still felt sick and in April 2013 insisted on a pregnancy test. When it was discovered that she was seven months pregnant, nobody could believe it. 
-	 Due to Christina's preeclampsia the birth was stimulated two weeks later and Ava was born!</p>
-	 <p>She was a completely healthy baby, but a month later Christina's
-	 cancer returned with vengeance. She fought valiantly and amazed everybody with her determination and strength.
-	 Christina kept withstanding one round of chemo after another while living life to the fullest as a mother, a daughter, a sister, a wife, and a friend.
-	 Her spirit was never defeated and her spiritual grace touched the hearts of thousands, but on October 26 2014 her body reached the limit of its resillience.
-	 Her beautiful spirit departed two months before her forthieth birthday, but a part of it contimues the journey on earth in her beautiful daughters - Alyssa
-	 (from the first marriage) and Ava. This fundraiser is for Ava's education fund. It takes a village to raise a child and we hope that you join the village of Ava's friends. 
-	 Thank you and God bless your heart.</p>
-	 <br>
-	 --Ernesto Monne
-	   
 
-
-	 
-
-
-
+<p>Ava is a miracle child of Christina Newman and Ernesto Monne. Her parents were married in April 26 2008. They wished for a child but for a while they had none. Than in December 2010 a tragedy struck the family - Christina was diagnosed with triple negative breast cancer. She underwent surgeries and chemotherapy. She was told that she won't be able to have children due to the side effects of the treatment.</p>
+<p>In September 2012 Christina's chemo was over, but she continued taking an experimental drug, a parp inhibitor. She felt sick and didn't understand why she can't lose weight. The doctors kept telling her it was due to the chemo's side effects. In February 2013 Christina was declared cancer free. She still felt sick and in April 2013 insisted on a pregnancy test. When it was discovered that she was seven months pregnant, nobody could believe it. Due to Christina's preeclampsia the birth was stimulated two weeks later and Ava was born!</p>
+<p>
+ She was a completely healthy baby, but a month later Christina's cancer returned with vengeance. She fought valiantly and amazed everybody with her determination and strength. Christina kept enduring one round of chemo after another while living life to the fullest as a mother, a daughter, a sister, a wife, and a friend. Her spirit was never defeated and her spiritual grace touched the hearts of thousands, but on October 26 2014 her body reached the limit of its resilience. Her beautiful spirit departed two months before her fortieth birthday, but a part of it continues the journey on earth in her beautiful daughters - Alyssa (from the first marriage) and Ava.</p>
+<p>
 
     </div>
   </body>
